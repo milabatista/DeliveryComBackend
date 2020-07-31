427eae13b13990cb8b76a8b5756d625559f905b7
Camila Tarifa <mika.tarifa@gmail.com>
2020-07-31 19:35:46 -0300

    deu tudo certo aleluia

---
+++ app/Restaurant.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant extends Model
+{
+    protected $fillable = ['name', 'type', 'phone', 'cellphone', 'whatsup', 'img_url', 'address', 'lat', 'lng'];
+
+    public function Menus(){
+        $this->belongsToMany(\App\Menus::class);
+    }
+}
